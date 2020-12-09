62e839ea6fbec7e71677be78bcc23512fa156e4b
jzm17173 <823458264@qq.com>
2020-12-09 21:07:12 +0800

    update

---
+++ nlptools/platform.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+import socket
+
+
+def get_host_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+        return ip
+    finally:
+        s.close()
+
+    return ""
+
+
+# 缓存
+ip = get_host_ip()
